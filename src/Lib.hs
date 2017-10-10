89dda8c3b92b0b085fdbfaee36af2ed67864f74f
Matt Moore <mattmoore@carbonhelix.com>
2017-10-09 21:26:21 -0500

    Some tweaks: Use function composition (.) instead of application ($).

--- src/Lib.hs
+++ src/Lib.hs
@@ -37,7 +37,7 @@ version = "photon 0.1.4\n"
 
 parse :: String -> [String] -> String
 parse key args
-  | elem key args = head $ tail (dropWhile (/= key) args)
+  | elem key args = head . tail $ dropWhile (/= key) args
   | otherwise     = ""
 
 parseBool :: String -> [String] -> Bool
@@ -58,10 +58,8 @@ parseHeader x = (hHdrCustom, value)
   where
     name = takeWhile (/= ':') x
     hHdrCustom :: HTypes.HeaderName
-    hHdrCustom = CI.mk $ B8.pack name
-    value = B8.pack
-          $ strip
-          $ dropWhile (== ':') (dropWhile (/= ':') x)
+    hHdrCustom = CI.mk . B8.pack $ name
+    value = B8.pack . strip $ dropWhile (== ':') (dropWhile (/= ':') x)
 
 parseHeaders :: [String] -> [HTypes.Header]
 parseHeaders x = map parseHeader (parseList "-H" x)
@@ -70,36 +68,36 @@ formatRFC1123 :: UTCTime -> T.Text
 formatRFC1123 = T.pack . formatTime defaultTimeLocale "%a, %d %b %Y %X GMT"
 
 httpTime :: UTCTime -> String
-httpTime x = T.unpack $ formatRFC1123 x
+httpTime x = T.unpack . formatRFC1123 $ x
 
 getPath :: String -> String
-getPath x = B8.unpack $ URITypes.extractPath $ B8.pack x
+getPath x = B8.unpack . URITypes.extractPath $ B8.pack x
 
-type EmptyMD5 = String
+emptyMD5 :: String
 emptyMD5 = "1B2M2Y8AsgTpgAmY7PhCfg=="
 
 contentMD5 :: String -> String
 contentMD5 x
   | x == ""   = emptyMD5
   | otherwise = B8.unpack
-              $ Base64.encode
-              $ md5DigestBytes
-              $ md5
+              . Base64.encode
+              . md5DigestBytes
+              . md5
               $ BL8.pack x
 
 findHeader :: String -> [Header] -> String
 findHeader headerName headers = value
   where
     hName :: HTypes.HeaderName
-    hName = CI.mk $ B8.pack headerName
+    hName = CI.mk . B8.pack $ headerName
     result = find (\x -> fst x == hName) headers
     value = case result of
       Nothing -> ""
-      Just y  -> B8.unpack $ snd y
+      Just y  -> B8.unpack . snd $ y
 
 canonicalForm :: String -> String -> String -> String -> String -> String
-canonicalForm method contentType contentMD5 uriPath timestamp =
-  intercalate "," [method, contentType, contentMD5, uriPath, timestamp]
+canonicalForm httpMethod contentType contentMD5 uriPath timestamp =
+  intercalate "," [httpMethod, contentType, contentMD5, uriPath, timestamp]
 
 authHeader :: String -> String -> String -> HTypes.Header
 authHeader client key message = (HTypes.hAuthorization, value)
@@ -107,31 +105,31 @@ authHeader client key message = (HTypes.hAuthorization, value)
     key'     = BL8.pack key
     message' = BL8.pack message
     digest   = BL8.unpack
-             $ BL8.fromStrict
-             $ Base64.encode
-             $ BL8.toStrict
-             $ bytestringDigest
+             . BL8.fromStrict
+             . Base64.encode
+             . BL8.toStrict
+             . bytestringDigest
              $ hmacSha1 key' message'
     value    = B8.pack $ "APIAuth " ++ client ++ ":" ++ digest
 
 getBody :: String -> IO String
 getBody x = body
   where
-    body | startswith "@" x = readFile $ dropWhile (== '@') x
+    body | startswith "@" x = readFile . dropWhile (== '@') $ x
          | otherwise        = return x
 
 fetchHttp :: String -> String -> String -> String -> [HTypes.Header] -> String -> Bool -> IO String
-fetchHttp method url' client key headers body' pretty = do
-  let url            | startswith "http://"  url' = url'
-                     | startswith "https://" url' = url'
-                     | otherwise                  = "http://" ++ url'
+fetchHttp httpMethod url' client key headers body' pretty = do
+  let url            | "http://"  `isPrefixOf` url' = url'
+                     | "https://" `isPrefixOf` url' = url'
+                     | otherwise                    = "http://" ++ url'
 
-  request'          <- parseRequest $ method ++ " " ++ url
+  request'          <- parseRequest $ httpMethod ++ " " ++ url
   timestamp         <- getCurrentTime
   body              <- getBody body'
 
   let canonical      = canonicalForm
-                       method
+                       httpMethod
                        (findHeader "Content-Type" headers)
                        (contentMD5 body)
                        (getPath    url)
@@ -139,22 +137,22 @@ fetchHttp method url' client key headers body' pretty = do
 
       signedHeaders  = headers ++ [
                        (authHeader client key canonical),
-                       (HTypes.hDate,              B8.pack $ httpTime timestamp),
-                       (HTypes.hContentMD5,        B8.pack $ contentMD5 body)]
+                       (HTypes.hDate,       B8.pack . httpTime $ timestamp),
+                       (HTypes.hContentMD5, B8.pack . contentMD5 $ body)]
 
       requestHeaders | client == "" && key == "" = headers
                      | otherwise                 = signedHeaders
 
-      request        = setRequestMethod  (B8.pack method)
-                     $ setRequestHeaders requestHeaders
-                     $ setRequestBodyLBS (BL8.pack body)
+      request        = setRequestMethod  (B8.pack httpMethod)
+                     . setRequestHeaders requestHeaders
+                     . setRequestBodyLBS (BL8.pack body)
                      $ request'
 
   response          <- httpLbs request
 
   let responseBody = getResponseBody response
-      ppJson       = encodePretty $ (decode responseBody :: Maybe Value)
+      ppJson       = encodePretty (decode responseBody :: Maybe Value)
       result       | pretty == True = ppJson
                    | otherwise      = responseBody
 
-  return $ BU.toString $ BL8.toStrict result
+  return . BU.toString . BL8.toStrict $ result
